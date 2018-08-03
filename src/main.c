57ddf6cae551ffacd3becf2a2433711cefccff46
anlaneg <anlaneg@126.com>
2018-08-03 09:55:26 +0800

    add comments

--- src/main.c
+++ src/main.c
@@ -1,8 +1,11 @@
-int app_parse_args(int argc,char**argv)
-{
-	//todo
-	return 0;
-}
+#include <stdint.h>
+#include <stdio.h>
+
+#include "rte_eal.h"
+#include "rte_ethdev.h"
+
+int app_parse_args(int argc,char**argv);
+
 int main(int argc,char**argv)
 {
 	int ret;
@@ -21,5 +24,15 @@ int main(int argc,char**argv)
 	nb_ports = rte_eth_dev_count_avail();
 	if (nb_ports == 0)
 		rte_exit(EXIT_FAILURE, "No Ethernet ports - bye\n");
+	return 0;
+}
+
+int app_parse_args(int argc,char**argv)
+{
+	int i;
+	for(i = 0 ; i < argc; i++)
+	{
+		printf("argv[%d]=%s\n",i,argv[i]);
+	}
 	return 0;
 }
