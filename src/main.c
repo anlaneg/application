8cb843281892cf67b48aa1a1b20d8beb40c6eb85
anlaneg <anlaneg@126.com>
2018-08-03 23:55:15 +0800

    update main.c

--- src/main.c
+++ src/main.c
@@ -6,10 +6,34 @@
 
 int app_parse_args(int argc,char**argv);
 
+struct rte_mempool * app_pktmbuf_pool = NULL;
+/*
+ * Configurable number of RX/TX ring descriptors
+ */
+#define RTE_TEST_RX_DESC_DEFAULT 1024
+#define RTE_TEST_TX_DESC_DEFAULT 1024
+static uint16_t nb_rxd = RTE_TEST_RX_DESC_DEFAULT;
+static uint16_t nb_txd = RTE_TEST_TX_DESC_DEFAULT;
+#define MAX_PKT_BURST 512
+#define DEF_PKT_BURST 32
+#define MEMPOOL_CACHE_SIZE 256
+
+static struct rte_eth_conf port_conf = {
+	.rxmode = {
+		.split_hdr_size = 0,
+		.offloads = DEV_RX_OFFLOAD_CRC_STRIP,
+	},
+	.txmode = {
+		.mq_mode = ETH_MQ_TX_NONE,
+	},
+};
+
 int main(int argc,char**argv)
 {
 	int ret;
 	uint16_t nb_ports;
+	uint32_t nb_mbufs;
+	uint16_t portid;
 
 	ret = rte_eal_init(argc, argv);
 	if (ret < 0)
@@ -24,6 +48,119 @@ int main(int argc,char**argv)
 	nb_ports = rte_eth_dev_count_avail();
 	if (nb_ports == 0)
 		rte_exit(EXIT_FAILURE, "No Ethernet ports - bye\n");
+
+	/* create the mbuf pool */
+	nb_mbufs = RTE_MAX(nb_ports * (nb_rxd + nb_txd + MAX_PKT_BURST +
+			RTE_MAX_LCORE * MEMPOOL_CACHE_SIZE), 8192U);
+	app_pktmbuf_pool = rte_pktmbuf_pool_create("mbuf_pool", nb_mbufs,
+			MEMPOOL_CACHE_SIZE, 0, RTE_MBUF_DEFAULT_BUF_SIZE,
+			rte_socket_id());
+	if (app_pktmbuf_pool == NULL)
+		rte_exit(EXIT_FAILURE, "Cannot init mbuf pool\n");
+
+	RTE_ETH_FOREACH_DEV(portid) {
+			struct rte_eth_rxconf rxq_conf;
+			struct rte_eth_txconf txq_conf;
+			struct rte_eth_conf local_port_conf = port_conf;
+			struct rte_eth_dev_info dev_info;
+
+			//nb_ports_available++;
+
+			/* init port */
+			printf("Initializing port %u... ", portid);
+			fflush(stdout);
+			rte_eth_dev_info_get(portid, &dev_info);
+			if (dev_info.tx_offload_capa & DEV_TX_OFFLOAD_MBUF_FAST_FREE)
+				local_port_conf.txmode.offloads |=
+					DEV_TX_OFFLOAD_MBUF_FAST_FREE;
+			ret = rte_eth_dev_configure(portid, 1, 1, &local_port_conf);
+			if (ret < 0)
+				rte_exit(EXIT_FAILURE, "Cannot configure device: err=%d, port=%u\n",
+					  ret, portid);
+
+			ret = rte_eth_dev_adjust_nb_rx_tx_desc(portid, &nb_rxd,
+							       &nb_txd);
+			if (ret < 0)
+				rte_exit(EXIT_FAILURE,
+					 "Cannot adjust number of descriptors: err=%d, port=%u\n",
+					 ret, portid);
+
+			//rte_eth_macaddr_get(portid,&l2fwd_ports_eth_addr[portid]);
+
+			/* init one RX queue */
+			fflush(stdout);
+			rxq_conf = dev_info.default_rxconf;
+			rxq_conf.offloads = local_port_conf.rxmode.offloads;
+			ret = rte_eth_rx_queue_setup(portid, 0, nb_rxd,
+						     rte_eth_dev_socket_id(portid),
+						     &rxq_conf,
+						     app_pktmbuf_pool);
+			if (ret < 0)
+				rte_exit(EXIT_FAILURE, "rte_eth_rx_queue_setup:err=%d, port=%u\n",
+					  ret, portid);
+
+			/* init one TX queue on each port */
+			fflush(stdout);
+			txq_conf = dev_info.default_txconf;
+			txq_conf.offloads = local_port_conf.txmode.offloads;
+			ret = rte_eth_tx_queue_setup(portid, 0, nb_txd,
+					rte_eth_dev_socket_id(portid),
+					&txq_conf);
+			if (ret < 0)
+				rte_exit(EXIT_FAILURE, "rte_eth_tx_queue_setup:err=%d, port=%u\n",
+					ret, portid);
+
+#if 0
+			/* Initialize TX buffers */
+			tx_buffer[portid] = rte_zmalloc_socket("tx_buffer",
+					RTE_ETH_TX_BUFFER_SIZE(MAX_PKT_BURST), 0,
+					rte_eth_dev_socket_id(portid));
+			if (tx_buffer[portid] == NULL)
+				rte_exit(EXIT_FAILURE, "Cannot allocate buffer for tx on port %u\n",
+						portid);
+
+			rte_eth_tx_buffer_init(tx_buffer[portid], MAX_PKT_BURST);
+
+			ret = rte_eth_tx_buffer_set_err_callback(tx_buffer[portid],
+					rte_eth_tx_buffer_count_callback,
+					&port_statistics[portid].dropped);
+			if (ret < 0)
+				rte_exit(EXIT_FAILURE,
+				"Cannot set error callback for tx buffer on port %u\n",
+					 portid);
+#endif
+
+			/* Start device */
+			ret = rte_eth_dev_start(portid);
+			if (ret < 0)
+				rte_exit(EXIT_FAILURE, "rte_eth_dev_start:err=%d, port=%u\n",
+					  ret, portid);
+
+			printf("done: \n");
+
+			rte_eth_promiscuous_enable(portid);
+#if 0
+			printf("Port %u, MAC address: %02X:%02X:%02X:%02X:%02X:%02X\n\n",
+					portid,
+					l2fwd_ports_eth_addr[portid].addr_bytes[0],
+					l2fwd_ports_eth_addr[portid].addr_bytes[1],
+					l2fwd_ports_eth_addr[portid].addr_bytes[2],
+					l2fwd_ports_eth_addr[portid].addr_bytes[3],
+					l2fwd_ports_eth_addr[portid].addr_bytes[4],
+					l2fwd_ports_eth_addr[portid].addr_bytes[5]);
+
+			/* initialize port stats */
+			memset(&port_statistics, 0, sizeof(port_statistics));
+#endif
+		}
+
+	RTE_ETH_FOREACH_DEV(portid) {
+		printf("Closing port %d...", portid);
+		rte_eth_dev_stop(portid);
+		rte_eth_dev_close(portid);
+		printf(" Done\n");
+	}
+	printf("Bye...\n");
 	return 0;
 }
 
